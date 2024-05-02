8654f2fe6b65c412826ef55574e09930e18e7a42
DAVIDE CHIEREGATO <davide.chieregato96@gmail.com>
2024-05-03 00:25:44 +0200

    [GCPPythonProject] Unit tests

--- test/test_cloudClientConnector.py
+++ test/test_cloudClientConnector.py
@@ -6,28 +6,61 @@ import logger
 from unittest.mock import patch, MagicMock
 from cloudClientConnector import CloudStorageClient,BigQueryClient
 
-log = logger.logger()
+# log = logger.logger()
 
 class TestCloudStorageClient(unittest.TestCase):
+
+    # patch per mockare le due chiamate
     @patch('cloudClientConnector.storage.Client')
-    def test_connect(self, storage_client_mock):
-        # Crea un'istanza di CloudStorageClient e chiamo metodo connect
+    @patch('cloudClientConnector.Log')
+    def test_connect_success(self, mock_log, mock_storage_client):
+
+        # istanziamo un oggetto della classe CloudStorageClient su cui poi fare il test
         cloud_storage_client = CloudStorageClient()
+
+        # chiamiamo il metodo che vogliamo testare
         cloud_storage_client.connect()
 
-        # Verifica che il client di storage mockato sia stato creato
-        storage_client_mock.assert_called_once()
+        # Assert
+        mock_storage_client.assert_called_once()  # Verifica se storage.Client() è stato chiamato esattamente una volta
+        # mock_log.info.assert_called_once_with("GCS Client connected!")
 
-    def test_get_client(self):
-        storage_client_mock = MagicMock()
+    @patch('cloudClientConnector.storage.Client')
+    @patch('cloudClientConnector.Log')
+    def test_connect_failure(self, mock_log, mock_storage_client):
+
+        # instantiate
+        cloud_storage_client = CloudStorageClient()
+        mock_storage_client.side_effect = Exception("Connection error")
+
+        # act
+        cloud_storage_client.connect()
+
+        # assert
+        # mock_log.error.assert_called_once_with("Error connecting to storage client: Connection error")
+
+    @patch('cloudClientConnector.CloudStorageClient.connect')
+    def test_get_client_if_not_connected(self, mock_storage_client_connect):
 
         cloud_storage_client = CloudStorageClient()
-        cloud_storage_client.connect = MagicMock(return_value=None)
+        cloud_storage_client.get_client()
+        mock_storage_client_connect.assert_called_once()
 
-        cloud_storage_client.client = storage_client_mock
-        storage_client_instance = cloud_storage_client.get_client()
+    @patch('cloudClientConnector.storage.Client')
+    def test_get_client_if_connected(self, mock_storage_client):
 
-        self.assertEqual(storage_client_instance, storage_client_mock)
+        cloud_storage_client = CloudStorageClient()
+        cloud_storage_client.client = MagicMock()
+
+        client = cloud_storage_client.get_client()
+
+        self.assertEqual(client, cloud_storage_client.client)
+
+
+
+
+
+
 
 
 class TestBigQueryClient(unittest.TestCase):
