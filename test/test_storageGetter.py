34822a9a965a5d5bb05af90d770bda3a14cc6858
DAVIDE CHIEREGATO <davide.chieregato96@gmail.com>
2024-05-06 01:28:39 +0200

    [GCPPythonProject] Unit tests

--- test/test_storageGetter.py
+++ test/test_storageGetter.py
@@ -7,7 +7,7 @@ from cloudClientConnector import CloudStorageClient
 class TestBucketGetter(unittest.TestCase):
 
     @patch('storageGetter.CloudStorageClient.get_client')
-    def test_get_client_success(self, mock_get_client):
+    def test_get_client(self, mock_get_client):
         bucket_getter = BucketGetter("test_bucket")
         bucket_getter._BucketGetter__storage_client
         mock_get_client.assert_called_once()
@@ -15,9 +15,7 @@ class TestBucketGetter(unittest.TestCase):
     @patch('storageGetter.CloudStorageClient.get_client')
     @patch('logger.Log')
     def test_get_bucket_success(self, mock_log, mock_get_client):
-
         bucket_getter = BucketGetter("test_bucket")
-
         bucket = bucket_getter.get_bucket()
 
         mock_get_client.return_value.get_bucket.assert_called_once()
@@ -26,34 +24,32 @@ class TestBucketGetter(unittest.TestCase):
     @patch('storageGetter.CloudStorageClient.get_client')
     @patch('logger.Log.logger')
     def test_get_bucket_failure(self, mock_log, mock_get_client):
-
         mock_get_client.get_bucket.side_effect = Exception("Bucket not found")
         bucket_getter = BucketGetter("test_bucket")
-
         bucket = bucket_getter.get_bucket()
 
         # mock_log.error.assert_called_once_with("Bucket 'test_bucket' not exists.")
 
 
-
-
 class TestFileGetter(unittest.TestCase):
-    def test_get_file(self):
-        # Mocking: Bucket, BucketGetter and get_bucket
-        mock_bucket = MagicMock()
-        mock_bucket_getter = MagicMock()
-        mock_bucket_getter.get_bucket.return_value = mock_bucket
 
+    @patch('storageGetter.BucketGetter.get_bucket')
+    @patch('logger.Log')
+    def test_get_file_success(self, mock_log, mock_get_bucket):
+        file_getter = FileGetter("test_bucket","test_file")
+        file = file_getter.get_file()
 
-        file_getter = FileGetter(bucket_name="mock_bucket_name", file_name="mock_file_name")
-        file_getter._FileGetter__bucket = mock_bucket_getter
-        file_getter.get_file()
+        mock_get_bucket.return_value.blob("test_file").assert_called_once()
+        self.assertEqual(file, mock_get_bucket.return_value.blob("test_file"))
 
-        # TEST 1
-        mock_bucket_getter.get_bucket.assert_called_once()
-        # TEST 2
-        mock_bucket.blob.assert_called_once_with("mock_file_name")
-        #NO ASSERT EQUAL TEST because no returned value
+    @patch('storageGetter.CloudStorageClient.get_client')
+    @patch('logger.Log.logger')
+    def test_get_file_failure(self, mock_log, mock_get_bucket):
+        mock_get_bucket.blob.side_effect = Exception("File not found")
+        file_getter = FileGetter("test_bucket","test_file")
+        file = file_getter.get_file()
+
+        # mock_log.error.assert_called_once_with("File 'test_file' not exists.")
 
 
 if __name__ == '__main__':
