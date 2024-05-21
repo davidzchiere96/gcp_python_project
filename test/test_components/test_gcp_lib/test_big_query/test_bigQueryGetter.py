c7cd568856678a64796665a2e5afa35e8a2aace6
DAVIDE CHIEREGATO <davide.chieregato96@gmail.com>
2024-05-21 16:51:18 +0200

    [gcp_python_project] unit testing

--- test/test_components/test_gcp_lib/test_big_query/test_bigQueryGetter.py
+++ test/test_components/test_gcp_lib/test_big_query/test_bigQueryGetter.py
@@ -2,20 +2,20 @@
 # sys.path.append(r"C:\Users\ECHIERDF9\OneDrive - NTT DATA EMEAL\Desktop\gcp_python_project\gcp_python_project\src")
 
 import unittest
-import logger
+import components.logger
 from unittest.mock import MagicMock, patch
-from bigQueryGetter import TableGetter
+from components.gcp_lib.big_query.bigQueryGetter import TableGetter
 
 
 class TestTableGetter(unittest.TestCase):
 
-    @patch('bigQueryGetter.BigQueryClient.get_client')
+    @patch('components.gcp_lib.big_query.bigQueryGetter.BigQueryClient.get_client')
     def test_get_client(self, mock_get_client):
         table_getter = TableGetter("test_table")
         table_getter._TableGetter__big_query_client
         mock_get_client.assert_called_once()
 
-    @patch('bigQueryGetter.BigQueryClient.get_client')
+    @patch('components.gcp_lib.big_query.bigQueryGetter..BigQueryClient.get_client')
     @patch('logger.Log')
     def test_get_table_success(self, mock_log, mock_get_client):
         table_getter = TableGetter("test_table")
@@ -24,7 +24,7 @@ class TestTableGetter(unittest.TestCase):
         mock_get_client.return_value.get_table.assert_called_once()
         self.assertEqual(table, mock_get_client.return_value.get_table("test_table"))
 
-    @patch('bigQueryGetter.BigQueryClient.get_client')
+    @patch('components.gcp_lib.big_query.bigQueryGetter..BigQueryClient.get_client')
     @patch('logger.Log.logger')
     def test_get_table_failure(self, mock_log, mock_get_client):
         mock_get_client.get_table.side_effect = Exception("Table not found")
